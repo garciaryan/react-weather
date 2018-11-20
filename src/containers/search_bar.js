b1b8f04f508de53de9a436789dbbeb0069f911ba
Ryan Garcia <garciaryan007@yahoo.com>
2018-11-20 11:27:04 -0800

    binds inputChange method to this

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,10 +5,13 @@ export default class SearchBar extends Component {
     super(props);
 
     this.state = {term: ''};
+
+    //take existing "onInputChange" method, bind "this"(SearchBar), and replace with "onInputChange" that has correct context.
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(event){
-    console.log(event.target.value);
+    this.setState({ term: event.target.value });
   }
 
   render(){
